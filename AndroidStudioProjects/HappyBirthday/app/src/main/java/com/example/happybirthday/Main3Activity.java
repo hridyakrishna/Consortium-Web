7c317bacff320a8732689d913bf53a0c9f710687
hridya <hridyakrishnar127@gmail.com>
2019-01-17 20:58:39 +0530

    android app added

---
+++ AndroidStudioProjects/HappyBirthday/app/src/main/java/com/example/happybirthday/Main3Activity.java
@@ -0,0 +1,15 @@
+package com.example.happybirthday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
---
+++ AndroidStudioProjects/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.happybirthday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void nextactivity (View view){
+        Intent intent = new Intent (this, nextactivity.class);
+        startActivity(intent);
+    }
+
+}
+

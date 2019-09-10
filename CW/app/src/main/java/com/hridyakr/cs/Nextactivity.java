36b2534506c2bb3d08af487e7fa6e72beb855d32
hridya <hridyakrishnar127@gmail.com>
2019-09-11 00:28:25 +0530

    Radio button and spinner added

--- CW/app/src/main/java/com/hridyakr/cs/Nextactivity.java
+++ CW/app/src/main/java/com/hridyakr/cs/Nextactivity.java
@@ -3,13 +3,39 @@ package com.hridyakr.cs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
-public class Nextactivity extends AppCompatActivity {
+public class Nextactivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] talents = { "Select", "Tailoring", "Painting", "Carpentry", "Writing", "Orating", "Sports", "Domestic Works", "Farming", "Beautician", "Artist", "Dance", "Music"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,talents);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),talents[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
     }
 
 }
+
+
+
